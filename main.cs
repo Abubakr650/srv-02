7d923660732a72bb34913343b663f52250a72f8f
Abubakr650 <abubakr650@outlook.sa>
2025-11-27 04:09:04 +0300

    Uncomment and comment listener prefix lines

--- main.cs
+++ main.cs
@@ -18,10 +18,10 @@ class Program
         };
 
         var listener = new HttpListener();
-        //listener.Prefixes.Add("http://0.0.0.0:8002/");
+        listener.Prefixes.Add("http://0.0.0.0:8002/");
         listener.Prefixes.Add(string.Format("http://*:8002/"));
         // listener.Prefixes.Add(string.Format("http://13.60.181.5:8002/")); 
-         listener.Prefixes.Add(string.Format("http://13.62.98.15:8002/")); 
+        // listener.Prefixes.Add(string.Format("http://13.62.98.15:8002/")); 
         listener.Start();
 
         try
